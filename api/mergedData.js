3226921eb621d21106d6fc0109f2fd29f379726e
Evan Breland <evan.breland@gmail.com>
2022-11-25 18:25:46 -0600

    User, Create, Update,Delete, etc.

--- api/mergedData.js
+++ api/mergedData.js
@@ -1,1 +1,30 @@
 // for merged promises
+// import client from '../utils/client';
+import { getSingleAuthor, getAuthorBooks, deleteSingleAuthor } from './authorData';
+import { getSingleBook, deleteBook } from './bookData';
+
+// const endpoint = client.databaseURL;
+
+const getBookDetails = (firebaseKey) => new Promise((resolve, reject) => {
+  getSingleBook(firebaseKey).then((bookObj) => {
+    getSingleAuthor(bookObj.author_id).then((authorObject) => resolve({ ...bookObj, authorObject }));
+  }).catch(reject);
+});
+
+const getAuthorDetails = (firebaseKey) => new Promise((resolve, reject) => {
+  getSingleAuthor(firebaseKey).then((authorObject) => {
+    getAuthorBooks(firebaseKey).then((booksArray) => resolve({ ...authorObject, booksArray }));
+  }).catch(reject);
+});
+
+const deleteAuthorBooksRelationship = (firebaseKey) => new Promise((resolve, reject) => {
+  getAuthorBooks(firebaseKey).then((authorBooksArray) => {
+    const deleteBookPromises = authorBooksArray.map((book) => deleteBook(book.firebaseKey));
+
+    Promise.all(deleteBookPromises).then(() => {
+      deleteSingleAuthor(firebaseKey).then(resolve);
+    });
+  }).catch(reject);
+});
+
+export { getBookDetails, getAuthorDetails, deleteAuthorBooksRelationship };
